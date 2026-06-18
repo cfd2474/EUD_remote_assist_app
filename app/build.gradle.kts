6b36bd203a77d5cc19097fc31bc33dcdc0f93596
cfd2474 <m.leckliter@cfa3757.org>
2026-06-18 02:43:04 -0700

    build: bump version to v2.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.cfd2474.eudremoteassist"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "2.0.1"
+        versionCode = 2
+        versionName = "2.0.2"
         buildConfigField("String", "GIT_HASH", "\"${gitCommitHash.get()}\"")
     }
     buildFeatures {
