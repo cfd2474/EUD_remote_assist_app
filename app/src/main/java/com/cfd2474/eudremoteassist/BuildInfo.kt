767d61dc631dce56518ed1ca33712d371b19bd78
cfd2474 <m.leckliter@cfa3757.org>
2026-06-21 14:40:39 -0700

    fix: sync BuildInfo version with BuildConfig

--- app/src/main/java/com/cfd2474/eudremoteassist/BuildInfo.kt
+++ app/src/main/java/com/cfd2474/eudremoteassist/BuildInfo.kt
@@ -1,7 +1,7 @@
 package com.cfd2474.eudremoteassist
 
 object BuildInfo {
-    const val VERSION_NAME = "2.0.8"
+    val VERSION_NAME = BuildConfig.VERSION_NAME
     const val LABEL = "v2-doc-rebuild"
     val GIT_HASH: String = BuildConfig.GIT_HASH
 }
